74a06fec57c6df029bfcc2f4b59d5325e4f52073
Austin Jones <aj@austo.io>
2017-04-13 23:10:58 -0700

    Testing character selection response

--- app/controllers/interactiveMessages/interactiveMessages.js
+++ app/controllers/interactiveMessages/interactiveMessages.js
@@ -1,5 +1,8 @@
+"use strict";
 
 
+var Character = require('../../models/Character').Character;
+var Firebase = require('../../libraries/firebase').Firebase;
 
 
 
@@ -8,10 +11,41 @@ exports.InteractiveMessages = (req, res, next) => {
     var messagePayload = JSON.parse(req.body.payload);
 
     console.log('Incoming interactive message, parsed body: ', messagePayload);
-    
-    var slackResponse = {
-        "text": 'received input'
-    };
 
-    res.status(200).send(slackResponse);
+    var messageResponse = determineResponse();
+
+
+
+    //Read callback to determine what path to take
+    function determineResponse(){
+        switch(messagePayload){
+
+            case 'characterClass':
+
+                var charProps = {
+                    character: {
+                        strength: 15,
+                        stamina: 10,
+                        class: messagePayload.actions[0].value
+                    }
+                };
+
+                //write to DB
+                var firebase = new Firebase();
+                
+                firebase.create(charProps);
+
+                //Respond with next question
+
+                return {
+                    "text": "you made your selection"
+                };
+
+                break;
+
+            default:
+        }
+    }
+
+    res.status(200).send(messageResponse);
 };
