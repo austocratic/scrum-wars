9b07521cbe0355e1a50ba679a822726c0fb142b2
Austin Jones <aj@austo.io>
2017-04-28 21:43:57 -0700

    Testing image getting

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 
 var profile = require('../app/controllers/commands/profile');
 
+var stockImage = require('../app/assets/wizardImage.jpg');
+
 
 var commands = require('../app/controllers/commands').commands;
 var interactiveMessages = require('../app/controllers/interactiveMessages').interactiveMessages;
@@ -14,6 +16,10 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+router.get('/image', function(req, res, next) {
+    res.sendFile(stockImage);
 });
 
 router.post('/api/commands', (req, res, next) => {
