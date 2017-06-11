df6023d534087f6c73b7b358af29a85c950396e8
Austin Jones <aj@austo.io>
2017-06-11 14:41:35 -0700

    testing character class

--- app/controllers/interactions/characterProfile.js
+++ app/controllers/interactions/characterProfile.js
@@ -38,6 +38,9 @@ exports.characterProfile = payload => {
         
         localCharacter.setByProperty('user_id', userID)
             .then(()=>{
+
+                console.log('set local character in characterProfile, props: '. JSON.stringify(localCharacter.props));
+
                 //Array of stat keys
                 var statKeys = Object.keys(localCharacter.props);
 
@@ -82,58 +85,4 @@ exports.characterProfile = payload => {
             .catch(err=>{
                 console.log('error when calling setByProperty:', err);
             });
-
-
-        /*
-        //Use Slack user ID to lookup the user's character
-        firebase.get('character', 'user_id', userID)
-            .then( character => {
-
-                //Convert the returned object into array of character IDs.  This works since the query only returns one result
-                var characterID = Object.keys(character)[0];
-
-                var characterStats = character[characterID];
-
-                //Array of stat keys
-                var statKeys = Object.keys(characterStats);
-                
-                    template.attachments[0].image_url = "https://scrum-wars.herokuapp.com/assets/fullSize/" + characterStats.class_id + ".jpg";
-
-                    //Iterate through the stat keys
-                    template.attachments[1].fields = statKeys.map( key =>{
-
-                        return {
-                            "title": key,
-                            "value": characterStats[key],
-                            "short": true
-                        };
-                    });
-                
-                    //Interactive portion of profile menu
-                    template.attachments[2] = {
-                        
-                        "callback_id": "profileOptionSelection",
-                        "fallback": "Unable to load inventory buttons",
-                        "actions": [{
-                            "name": "inventory",
-                            "text": "Inventory",
-                            "style": "default",
-                            "type": "button",
-                            "value": "inventory"
-                        },
-                        {
-                            "name": "equipment",
-                            "text": "Equipped Items",
-                            "style": "default",
-                            "type": "button",
-                            "value": "equipment"
-                        }]
-                    };
-
-                    console.log('Final template: ', JSON.stringify(template));
-
-                    resolve(template);
-
-            });
-    })*/
 };
