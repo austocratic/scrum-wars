75299edf8fded0d329aa444f5d32fa0f62928fd7
Austin Jones <aj@austo.io>
2017-06-15 00:21:18 -0700

    troubleshooting equip function

--- app/controllers/Character.js
+++ app/controllers/Character.js
@@ -179,6 +179,7 @@ class Character extends FirebaseBaseController{
                                                 resolve()
                                             });
                                     } else {
+                                        resolve()
 
                                     }
                                 })
