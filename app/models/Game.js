204004d60d8594443057ab08ca144d405c5a42a0
Austin Jones <aj@austo.io>
2017-11-08 16:22:50 -0800

    cleave is not showing up.  Debugging

--- app/models/Game.js
+++ app/models/Game.js
@@ -166,15 +166,20 @@ class Game {
                             //If the action has ongoing effects, process them
                             if (effectAction.props.ongoing_effects){
                                 effectAction.props.ongoing_effects.forEach( eachOngoingEffect =>{
+
+                                    console.log('DEBUG it is number_turns: ', currentMatch.props.number_turns);
+                                    console.log('DEBUG it is turn_applied: ', eachEffect.turn_applied);
+                                    console.log('DEBUG activating function: ', eachOngoingEffect.functionName);
+                                    console.log('DEBUG eachEffect.turn_effect_processed: ', eachEffect.turn_effect_processed);
+
+
                                     //Check if the action has an ongoing effect that should apply to the current turn.
                                     //In order to get the relative turn number take the current turn - the turn the action was applied
                                     if(eachOngoingEffect.active_on_turn.includes(currentMatch.props.number_turns - eachEffect.turn_applied) &&
                                         !eachEffect.turn_effect_processed.includes(currentMatch.props.number_turns - eachEffect.turn_applied)
                                     ){
-
-                                        console.log('DEBUG it is relative turn: ', currentMatch.props.number_turns - eachEffect.turn_applied);
                                         console.log('DEBUG the effect SHOULD be applied this turn!  Activating it!');
-                                        console.log('DEBUG activating function: ', eachOngoingEffect.functionName);
+
 
                                         //Validate that the effect has not already been processed this turn
 
