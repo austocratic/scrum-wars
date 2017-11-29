4c38256f29e1a57e488b1774f61b6a1f44d968ec
Austin Jones <aj@austo.io>
2017-11-29 15:48:48 -0800

    added logging to back button processing

--- app/controllers/backButton.js
+++ app/controllers/backButton.js
@@ -24,12 +24,17 @@ const modifyCallbackForBack = slackCallback => {
         .splice( slackCallbackElements.length - 3, 3);
 
     //Remove the value from the key:value
-    lastKeyValue.pop();
+    //lastKeyValue.pop();
+    lastKeyValue
+        .splice(lastKeyValue.length - 2, 2);
     
     //If the callback had 3 game contexts, then there will be no slackCallbackElements to join, return the last 1st game context:
     if (slackCallbackElements.join("/").length === 0){
         return lastKeyValue[0];
     }
+
+    console.log('DEBUG modifyCallbackForBack, slackCallbackElements: ', slackCallbackElements);
+    console.log('DEBUG modifyCallbackForBack, lastKeyValue: ', lastKeyValue);
 
     //If there are more elements to join (does not hit if above), concatenate
     return slackCallbackElements.join("/") + "/" + lastKeyValue[0];
@@ -45,8 +50,7 @@ const modifyUserActionNameSelection = slackCallback => {
 
     //console.log('DEBUG slackCallbackElements: ', slackCallbackElements);
 
-    //take the previous element & split it into context:name:value
-    let lastKeyValue = slackCallbackElements[slackCallbackElements.length - 2]
+    let lastKeyValue = slackCallbackElements[slackCallbackElements.length - 3]
         .split(":");
 
     //console.log('DEBUG lastKeyValue: ', lastKeyValue);
