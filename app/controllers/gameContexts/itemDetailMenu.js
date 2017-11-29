39032888c8ade6a5965598f83c01d98cf56fc775
Austin Jones <aj@austo.io>
2017-11-28 16:48:24 -0800

    Missing helper

--- app/controllers/gameContexts/itemDetailMenu.js
+++ app/controllers/gameContexts/itemDetailMenu.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const updateCallback = require('../../helpers').updateCallback;
+const validateGameObjects = require('../../helpers').validateGameObjects;
 const Item = require('../../models/Item').Item;
 
 
