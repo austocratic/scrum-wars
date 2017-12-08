19575f55c614dc4d7cd5e28174a855172719a804
Austin Jones <aj@austo.io>
2017-12-07 18:42:22 -0800

    fix

--- app/middleware/formatSlackRequest.js
+++ app/middleware/formatSlackRequest.js
@@ -44,7 +44,7 @@ const modifyPayloadForReservedActions = (req) => {
 
             let slackCallbackElements = req.payload.callback_id.split("/");
 
-            let lastKeyValue = slackCallbackElements[slackCallbackElements.length - 3]
+            let lastKeyValue = slackCallbackElements[slackCallbackElements.length - 2]
                 .split(":");
 
             //return lastKeyValue[1];
@@ -74,32 +74,32 @@ const modifyCallbackForBack = slackCallback => {
     //Split up the callback string
     let slackCallbackElements = slackCallback.split("/");
 
-    //console.log('DEBUG modifyCallbackForBack, slackCallbackElements: ', slackCallbackElements);
+    console.log('DEBUG modifyCallbackForBack, slackCallbackElements: ', slackCallbackElements);
 
     //If the callback string is less than 4 elements then it has at most 3 game contexts.
     //In order to go "back" we need to get the context that is 2 elements down.
-    if (slackCallbackElements.length < 4) {
-        //console.log('DEBUG slackCallbackElements was less than 3');
-        return slackCallbackElements[slackCallbackElements.length - 3]
+    if (slackCallbackElements.length < 3) {
+        console.log('DEBUG slackCallbackElements was less than 3');
+        return slackCallbackElements[slackCallbackElements.length - 2]
             .split(":")[0];
     }
 
     //take the last element & split it into context:selection
-    let lastKeyValue = slackCallbackElements[slackCallbackElements.length - 3]
+    let lastKeyValue = slackCallbackElements[slackCallbackElements.length - 2]
         .split(":");
 
     //remove the last element from the array (the current context)
     slackCallbackElements
-        .splice( slackCallbackElements.length - 3, slackCallbackElements.length);
+        .splice( slackCallbackElements.length - 2, slackCallbackElements.length);
 
-    //console.log('DEBUG slackCallbackElements after splice: ', slackCallbackElements);
+    console.log('DEBUG slackCallbackElements after splice: ', slackCallbackElements);
 
     //Remove the value from the key:value
     //lastKeyValue.pop();
     lastKeyValue
         .splice(lastKeyValue.length - 2, 2);
 
-    //console.log('DEBUG lastKeyValue after splice: ', lastKeyValue);
+    console.log('DEBUG lastKeyValue after splice: ', lastKeyValue);
 
     //If the callback had 3 game contexts, then there will be no slackCallbackElements to join, return the last 1st game context:
     if (slackCallbackElements.join("/").length === 0){
