9635aa5228135783301fc7c55f16ad5d53fd3ef9
Austin Jones <aj@austo.io>
2018-03-14 14:04:56 -0700

    Removing global_settings object.  Moving settings into their models.  I.E match.settings

--- app/controllers/gameControllers/checkForMatchStart.js
+++ app/controllers/gameControllers/checkForMatchStart.js
@@ -25,7 +25,6 @@ const checkForMatchStart = (gameObjects) => {
 
     //console.log('currentHour: ', currentHour);
     //console.log('currentDay: ', currentDay);
-    //console.log('gameObjects.game.state.global_settings.match.schedule[currentDay].start_hour: ', gameObjects.game.state.global_settings.match.schedule[currentDay].start_hour);
 
     //Check if the most recent match was already started today
     if(gameObjects.lastMatch.props.date_started){
@@ -44,34 +43,37 @@ const checkForMatchStart = (gameObjects) => {
     }
 
     //If today does not have active flag or is undefined, escape
-    if(_.get(gameObjects, `game.state.global_settings.match.schedule[${currentDay}].active`, 0) === 0){
+    if(_.get(gameObjects, `game.state.match.settings.schedule[${currentDay}].active`, 0) === 0){
         console.log('Did not start the match because currentDay is not active or undefined');
         return;
     }
 
-    if (!_.has(gameObjects, `game.state.global_settings.match.schedule[${currentDay}].start_hour`)) {
-        throw new Error('global_settings.match.schedule is active for today but has no start time defined')
+    if (!_.has(gameObjects, `game.state.match.settings.schedule[${currentDay}].start_hour`)) {
+        throw new Error('match schedule schedule is active for today but has no start time defined')
     }
 
-    //Check if current hour is greater than the scheduled start hour
-    if (currentHour > gameObjects.game.state.global_settings.match.schedule[currentDay].start_hour){
-        console.log('Time to start the match!');
-
-        gameObjects.currentMatch.start(gameObjects.game.getCharacterIDsInZone(gameObjects.currentMatch.props.zone_id));
-
-        slack({
-            "username": gameObjects.requestZone.props.zone_messages.name,
-            "icon_url": gameObjects.game.baseURL + gameObjects.game.thumbImagePath + gameObjects.requestZone.props.zone_messages.image + '.bmp',
-            //TODO dont hardcode the arena
-            "channel": ("#arena"),
-            "attachments": [{
-                "text": "Prepare for battle! The match begins!",
-                "color": gameObjects.game.menuColor
-            }]
-        });
+    //Check if current hour is less than the scheduled start hour, if so escape
+    if (currentHour < gameObjects.game.state.match.settings.schedule[currentDay].start_hour) {
+        console.log(`Current hour ${currentHour} is less than start hour ${gameObjects.game.state.match.settings.schedule[currentDay].start_hour}, don't start`);
+        return;
     }
 
-    console.log(`Did not start the match!`);
+    //Start the match
+    gameObjects.currentMatch.start(gameObjects.game.getCharacterIDsInZone(gameObjects.currentMatch.props.zone_id));
+
+    //Determine what type of match it is in order to announce it
+
+    //Notify of match start
+    slack({
+        "username": gameObjects.requestZone.props.zone_messages.name,
+        "icon_url": gameObjects.game.baseURL + gameObjects.game.thumbImagePath + gameObjects.requestZone.props.zone_messages.image + '.bmp',
+        //TODO dont hardcode the arena
+        "channel": ("#arena"),
+        "attachments": [{
+            "text": "Prepare for battle! The match begins!",
+            "color": gameObjects.game.menuColor
+        }]
+    });
 };
 
 
