9d38614cd5250bd9583cfad33146efa287692e4c
Austin Jones <aj@austo.io>
2018-05-27 09:41:46 -0700

    testing overhaul of action availability function

--- app/controllers/gameContextControllers/selectActionMenu.js
+++ app/controllers/gameContextControllers/selectActionMenu.js
@@ -351,13 +351,35 @@ const basicMelee = gameObjects => {
         'slackResponseTemplate'
     ]);
 
+    console.log('DEBUG mana: ', gameObjects.playerCharacter.props.mana_points);
+    console.log('DEBUG mana cost: ', gameObjects.actionTaken.props.mana_cost);
+    console.log('DEBUG stamina: ', gameObjects.playerCharacter.props.stamina_points);
+    console.log('DEBUG stamina cost: ', gameObjects.actionTaken.props.stamina_cost);
+
     gameObjects.actionTaken = new Action(gameObjects.game.state, gameObjects.userActionValueSelection);
 
+    if (!gameObjects.playerCharacter.checkMana(gameObjects.actionTaken.props.mana_cost) && !gameObjects.playerCharacter.checkStamina(gameObjects.actionTaken.props.stamina_cost)){
+        return {
+            "text": `_You don't have enough mana or stamina to use ${gameObjects.actionTaken.props.name}_`
+        }
+    }
+    if (!gameObjects.playerCharacter.checkMana(gameObjects.actionTaken.props.mana_cost)){
+        return {
+            "text": `_You don't have enough mana to use ${gameObjects.actionTaken.props.name}_`
+        }
+    }
+    if (!gameObjects.playerCharacter.checkStamina(gameObjects.actionTaken.props.stamina_cost)){
+        return {
+            "text": `_You don't have enough stamina to use ${gameObjects.actionTaken.props.name}_`
+        }
+    }
+
+    /*
     if (!gameObjects.playerCharacter.isActionAvailable(gameObjects.actionTaken)) {
         return {
             "text": `_You don't have enough action points to use ${gameObjects.actionTaken.props.name}_`
         }
-    }
+    }*/
 
     return targetSelection(gameObjects);
 };
