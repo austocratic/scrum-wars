bce82093b673254ca223aba8c6c2f6b42abc10ba
Austin Jones <aj@austo.io>
2018-09-17 20:58:24 -0700

    Add cron library and scheduled logging job for testing

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
+"use strict";
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+const Scheduler = require('./app/libraries/scheduler').Scheduler;
 
-var app = express();
+const index = require('./routes/index');
 
+const app = express();
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,9 +20,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
+//Create a cron object to process scheduled tasks
+let cron = new Scheduler({
+    delay: 60000,
+    processes: [
+        {
+            action: ()=>{
+                console.log('Scheduler triggered!')
+            }
+        },
+    ]
+});
+
+//Start the cron
+cron.start();
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
---
+++ app/libraries/scheduler.js
@@ -0,0 +1,66 @@
+'use strict';
+
+class Scheduler {
+    constructor(args) {
+        this.processes = args.processes;
+        this.delay = args.delay;
+        //this.interval = '';
+
+        //arguments[1] = arguments[1] ? arguments[1] : 0; // ternary operator
+    }
+
+    _iterateAllProcesses() {
+        this.processes.forEach( process => {
+            process.action();
+        })
+    }
+
+    start() {
+        this.interval = setInterval( () => {
+            this._iterateAllProcesses()
+        }, this.delay);
+    }
+
+    /*
+    stopCron() {
+        clearInterval(this.interval);
+    }
+
+    addTask(name, process) {
+
+        var task = new Process(name, process);
+
+        task.setProcessLogID(this._addToProcessLog(task));
+    }
+
+    _addToProcessLog(process){
+        return (this.processList.push(process) - 1);
+    }*/
+}
+
+/*
+class Process {
+    constructor(name, action) {
+        this.name = name;
+        this.action = action;
+    }
+
+    setProcessLogID(id) {
+        this.processLogID = id;
+    }
+
+    //TODO: add an 'active' property.  cron will only call Processes with active = true
+}*/
+
+
+module.exports = {
+    Scheduler
+};
+
+
+
+
+
+
+
+
